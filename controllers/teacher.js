0539df5d0d94b679ca6d443e245512bde13e2dee
Rachit <rachitdev45@gmail.com>
2021-06-12 16:26:06 +0530

    Added new routes to teacher controller

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -3,8 +3,12 @@ const express = require('express')
 const {
     createTeacher,
     signInTeacher,
+    deleteAccount,
     createClass,
-    deleteClass
+    deleteClass,
+    addStudent,
+    removeStudent,
+    getStudents
 } = require('../services/teacher')
 
 const auth = require('../middleware/auth')
@@ -69,6 +73,26 @@ router.post('/teacher/signin', async (req, res) => {
     }
 })
 
+router.delete('/teacher', auth, async (req, res) => {
+
+    try {
+
+        // extract the teacher id passed by the auth middleware
+        const teacherID = res.locals.id
+
+        await deleteAccount(teacherID)
+
+        res.status(200).send()
+
+    } catch (e) {
+
+        // for any error
+        console.log(e)
+
+        res.status(400).send()
+    }
+})
+
 router.post('/teacher/create', auth, async (req, res) => {
 
     try {
@@ -92,7 +116,7 @@ router.post('/teacher/create', auth, async (req, res) => {
 router.delete('/teacher/:classid', auth, async (req, res) => {
 
     try {
-        
+
         // take the class id from query params
         const classID = req.params.classid
 
@@ -101,7 +125,7 @@ router.delete('/teacher/:classid', auth, async (req, res) => {
 
         const rowsAffected = await deleteClass(classID, teacherID)
 
-        if (rowsAffected === 0){
+        if (rowsAffected === 0) {
 
             // case when no such class was found for the teacher
             return res.status(406).send()
@@ -116,5 +140,99 @@ router.delete('/teacher/:classid', auth, async (req, res) => {
     }
 
 })
+
+router.post('/teacher/:classid/:studentid', auth, async (req, res) => {
+
+    try {
+
+        // take the class id from query params
+        const classID = req.params.classid
+
+        // take the student id from query params
+        const studentID = req.params.studentid
+
+        // extract the teacher id passed by the auth middleware
+        const teacherID = res.locals.id
+
+        const isValid = await addStudent(classID, studentID, teacherID)
+
+        // if the teacher has no such class associated with her/him
+        if (!isValid) {
+            return res.status(406).send()
+        }
+
+        res.status(200).send()
+
+    } catch (e) {
+
+        console.log(e)
+        // if any error
+        res.status(400).send()
+    }
+})
+
+router.delete('/teacher/:classid/:studentid', auth, async (req, res) => {
+
+    try {
+
+        // take the class id from query params
+        const classID = req.params.classid
+
+        // take the student id from query params
+        const studentID = req.params.studentid
+
+        // extract the teacher id passed by the auth middleware
+        const teacherID = res.locals.id
+
+        const isValid = await removeStudent(classID, studentID, teacherID)
+
+        // if the teacher has no such class associated with her/him
+        // or student is not enrolled in this class
+        if (!isValid) {
+
+            return res.status(406).send()
+        }
+
+        res.status(200).send()
+
+
+    } catch (e) {
+
+        console.log(e)
+        // if any error
+        res.status(400).send()
+    }
+})
+
+router.post("/teacher/:classid", async (req, res) => {
+
+    try {
+
+        // take the class id from query params
+        const classID = req.params.classid
+
+        // extract the teacher id passed by the auth middleware
+        const teacherID = res.locals.id
+
+        const details = await getStudents(classID, teacherID)
+
+        // if the teacher has no such class associated with her/him
+        if (details === null) {
+
+            return res.status(406).send()
+        }
+
+        res.status(200).json(details)
+
+    } catch (e) {
+
+        console.log(e)
+        // if any error
+        res.status(400).send()
+
+    }
+
+})
+
 
 module.exports = router
--- services/teacher.js
+++ services/teacher.js
@@ -65,6 +65,14 @@ const signInTeacher = async (email, password) => {
 
 }
 
+const deleteAccount = async (teacherID) => {
+
+    await pool.query(
+        `DELETE FROM teacher WHERE teacher_id = $1`,
+        [teacherID])
+
+}
+
 const createClass = async (teacher_id, subject) => {
 
     await pool.query(
@@ -81,9 +89,90 @@ const deleteClass = async (classID, teacherID) => {
 
     return details.rowCount
 }
+
+const addStudent = async (classID, studentID, teacherID) => {
+
+    // to check if the class is associated with the teacher 
+    const isClass = await pool.query(
+        `SELECT * FROM class 
+        WHERE class_id = $1 AND fk_teacher_id = $2 `,
+        [classID, teacherID])
+
+    // if no such class associated with the teacher is found
+    if (isClass.rowCount === 0) {
+
+        return false
+    }
+
+    await pool.query(
+        `INSERT INTO class_student (fk_class_id, fk_student_id)
+        VALUES ($1, $2)`,
+        [classID, studentID])
+
+    return true
+}
+
+const getStudents = async (classID, teacherID) => {
+
+    // to check if the class is associated with the teacher 
+    const isClass = await pool.query(
+        `SELECT * FROM class 
+        WHERE class_id = $1 AND fk_teacher_id = $2 `,
+        [classID, teacherID])
+
+    // if no such class associated with the teacher is found
+    if (isClass.rowCount === 0) {
+
+        return null
+    }
+
+    const details = await pool.query(
+        `SELECT student.first_name, student.last_name FROM class
+        JOIN class_student
+        ON class.class_id = class_student.fk_class_id
+        JOIN student
+        ON student.student_id = class_student.fk_student_id
+        WHERE class_student.fk_class_id = $1`,
+        [classID])
+
+    return details.rows
+} 
+
+const removeStudent = async (classID, studentID, teacherID) => {
+
+    // to check if the class is associated with the teacher 
+    const isClass = await pool.query(
+        `SELECT * FROM class 
+        WHERE class_id = $1 AND fk_teacher_id = $2 `,
+        [classID, teacherID])
+
+    // if no such class associated with the teacher is found
+    if (isClass.rowCount === 0) {
+
+        return false
+    }
+
+    const rowsAffected = await pool.query(
+        `DELETE FROM class_student WHERE 
+        fk_class_id = $1 AND fk_student_id = $2)`,
+        [classID, studentID])
+
+    // if the student was not enrolled in that class
+    if (rowsAffected === 0) {
+
+        return false
+    }
+
+    return true
+}
+
 module.exports = {
     createTeacher,
     signInTeacher,
+    deleteAccount,
     createClass,
-    deleteClass
+    deleteClass,
+    addStudent,
+    removeStudent,
+    getStudents
 }
